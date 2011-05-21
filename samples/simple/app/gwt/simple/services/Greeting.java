cc08a6b5c84118805e6f50ac5d1f3b75c094a093
Vincent Buzzano <vincent.buzzano@gmail.com>
2011-05-21 02:41:14 +0200

    update samples simple

---
+++ samples/simple/app/gwt/simple/services/Greeting.java
@@ -0,0 +1,25 @@
+package gwt.simple.services;
+
+import gwt.simple.client.GreetingService;
+import gwt.simple.shared.FieldVerifier;
+
+import play.modules.gwt2.GWT2Service;
+import play.modules.gwt2.GWT2ServicePath;
+
+
+import com.google.gwt.user.server.rpc.*;
+
+public class Greeting extends GWT2Service implements GreetingService {
+ 
+    public String greetServer(String name) {
+
+        // Verify that the input is valid. 
+        if (!FieldVerifier.isValidName(name)) {
+            // If the input is not valid, throw an IllegalArgumentException back to
+            // the client.
+            throw new IllegalArgumentException("Name must be at least 4 characters long");
+		}
+		
+        return "Hello " + name;
+    }
+}
